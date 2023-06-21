2b43ab882d7108958f5af7239361cd5ac0a87e5c
Martin Figueroa <supermacaco86@gmail.com>
2023-06-21 17:43:20 -0300

    weather main info

--- src/components/weatherApp.js
+++ src/components/weatherApp.js
@@ -1,24 +1,35 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import WeatherForm from "./weatherForm";
 
 export default function WeatherApp(){
     const [weather, setWeather] = useState(null);
 
-    async function loadInfo(city = "Buenos Aires"){
+    useEffect(()=>{
+        loadInfo()
+    }, [])
+
+    useEffect(()=>{
+        document.title = `Weather | ${weather?.location.name ?? ""} `
+    }, [weather])
+
+    async function loadInfo(city = 'London'){
         try {
-            const request = await fetch(`${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}`)
-        } catch () {
+            const request = await fetch(`${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`)
+            const json = await request.json()
+            console.log(json)
+            setWeather(json)
+        } catch (error) {
             
         }
     }
 
     function handleChangeCity(city){
         setWeather(null)
-        loadInfo()
+        loadInfo(city)
     }
 
     return <div>
         <WeatherForm onChangeCity={handleChangeCity}/>
-        <div>Info</div>
+        <div>{weather?.current.temp_c}</div>
     </div>
 }          
